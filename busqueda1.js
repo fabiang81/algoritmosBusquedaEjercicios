5a046ea2369f92ad4852a8e9fdd8e9c48490a33f
Fabian Gonzalez <fabian.gonzalez@MacBook-Pro-de-Fabian.local>
2020-08-04 23:15:27 -0400

    eliminada variable aux

--- busqueda1.js
+++ busqueda1.js
@@ -1,4 +1,4 @@
-var myArray = [6,1,8,2,3,6,3,2,5,6,8,8,6,8,8];
+var myArray = [6,1,8,2,3,6,3,2,5,6,8,8,6];
 
 var numeroMayor;
 var maximoRepeticiones=0;
@@ -6,11 +6,10 @@ var maximoRepeticiones=0;
 function buscarRepeticiones(array){
     var numero;
     var repeticiones=0;
-    var aux=0;
-    for(i=aux; i<array.length; i++){
+    for(i=0; i<array.length; i++){
         repeticiones = 0;
         numero = array[i];
-        for(j=aux+i; j<array.length; j++){
+        for(j=0+i; j<array.length; j++){
             if(numero === array[j]){
                 repeticiones++;
             }
